08b3c015a1087ba07c6c29d127b3fb4e64074637
Ernesto Miranda <ernomitrovic@zohomail.com>
2022-10-17 21:25:52 -0500

    Mercado Pago funcionando

--- main.php
+++ main.php
@@ -1,39 +1,54 @@
-<?php 
+<?php
 
-    require __DIR__ .  '/vendor/autoload.php';
-    MercadoPago\SDK::setAccessToken("TEST-5858676746087604-080521-cad403637c2434eba4561e6d31ed45c0-1168111390");
+require __DIR__ .  '/vendor/autoload.php';
+MercadoPago\SDK::setAccessToken("TEST-5858676746087604-080521-cad403637c2434eba4561e6d31ed45c0-1168111390");
 
-    // Para el valor del donativo
-    session_start();
+// Para el valor del donativo
+session_start();
 
-    $_SESSION["name"] = $_POST["name"];
-    $_SESSION["donation"] = $_POST["donation"];
+$_SESSION["name"] = $_POST["name"];
+$_SESSION["donation"] = $_POST["donation"];
 
-    $donativo = $_SESSION["donation"];
-    // Crea un objeto de preferencia
-    $preference = new MercadoPago\Preference();
+$donativo = $_SESSION["donation"];
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
 
-    // Crea un ítem en la preferencia
-    $item = new MercadoPago\Item();
-    $item->title = 'Donacion';
-    $item->quantity = 1;
-    $item->unit_price = $donativo;
-    $preference->items = array($item);
-    $preference->save();
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Donacion';
+$item->quantity = 1;
+$item->unit_price = $donativo;
+$preference->items = array($item);
+$preference->save();
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main page</title>
+    <title>Donacion Pago</title>
     <link rel="stylesheet" href="./styles/main.css">
     <script src="https://sdk.mercadopago.com/js/v2"></script>
 </head>
-<body> 
-    <h1>Gracias</h1>
+
+<body>
+    <h1>Gracias <?= "${_SESSION['name']}" ?></h1> <br>
+    <h2>Con tu ayuda podemos ayudar a más personas</h2> <br>
+    <p>Transacción 100% segura <br>
+        Si eres una empresa y deseas donar o requieres recibo deducible de tu donativo escríbenos a
+        <a href="mailto:procuracion.fondos@depmh.org">procuracion.fondos@depmh.org</a>
+    </p>
+    <div class="row">
+        <div class="column">
+            <img src="./img/Dona.JPG" alt="Dona" width="200">
+        </div>
+        <div class="column">
+            <img src="./img/Descubre.JPG" alt="Descubre" width="200">
+        </div>
+    </div>
 
     <div class="check-btn"></div>
 
@@ -44,13 +59,14 @@
 
         mp.checkout({
             preference: {
-            id: '<?= $preference->id?>'
+                id: '<?= $preference->id ?>'
             },
             render: {
-            container: '.check-btn',
-            label: 'Pagar',
+                container: '.check-btn',
+                label: 'Donar',
             }
         });
     </script>
 </body>
+
 </html>
